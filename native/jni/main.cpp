9edf8f33b1cff3a47b9735e0af8bf064c76b50f2
Takeda-senpai <deadboltsx303@gmail.com>
2022-12-31 21:01:03 +0700

    handle zygote restart

--- native/jni/main.cpp
+++ native/jni/main.cpp
@@ -13,15 +13,24 @@
 #include <string>
 #include <sched.h>
 #include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/prctl.h>
+#include <signal.h>
 
 using namespace std;
 
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "exec_wrapper", __VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "exec_wrapper", __VA_ARGS__)
 
+char buf[256];
+
+void unmount_orig(int num){
+    umount2(buf, MNT_DETACH);
+    exit(num);
+}
+
 int main(int argc, char **argv) {
     LOGD("exec wrapper start");
-    char buf[256];
     memset(buf, 0, sizeof(buf));
     if (readlink("/proc/self/exe", buf, sizeof(buf)) < 0) {
         LOGE("stat:%s", strerror(errno));
@@ -40,6 +49,14 @@ int main(int argc, char **argv) {
     string bak = string(_realpath) + ".orig";
     delete _realpath;
     if (getuid() == 0 && !mount(bak.data(), buf, nullptr, MS_BIND, nullptr)) {
+        if (fork() == 0){
+            // handle zygote restart
+            signal(SIGUSR1, unmount_orig);
+            signal(SIGTERM, SIG_IGN);
+            prctl(PR_SET_PDEATHSIG, SIGUSR1);
+            while (true) pause();
+            _exit(1);
+        }
         LOGD("exec:%s", buf);
         if (execve(buf, argv, environ)) {
             LOGE("execve:%s", strerror(errno));
