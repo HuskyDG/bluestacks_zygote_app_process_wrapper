7f1cebe8f31714708b3c28a68a1eb9a47367a27b
Takeda-senpai <deadboltsx303@gmail.com>
2022-12-31 21:11:42 +0700

    ensure process is zygote

--- native/jni/main.cpp
+++ native/jni/main.cpp
@@ -24,38 +24,59 @@ using namespace std;
 
 char buf[256];
 
-void unmount_orig(int num){
+void unmount_orig(int num) {
+    LOGD("zygote died");
     umount2(buf, MNT_DETACH);
     exit(num);
 }
 
+int parse_ppid(int pid = -1) {
+    char path[32];
+    int ppid;
+    if (pid > 0) sprintf(path, "/proc/%d/stat", pid);
+    else sprintf(path, "/proc/self/stat");
+    FILE *pfstat = fopen(path, "re");
+    if (!pfstat)
+        return -1;
+    // PID COMM STATE PPID .....
+    fscanf(pfstat, "%*d %*s %*c %d", &ppid);
+    fclose(pfstat);
+    return ppid;
+}
+
 int main(int argc, char **argv) {
     LOGD("exec wrapper start");
+    bool zygote = false;
     memset(buf, 0, sizeof(buf));
     if (readlink("/proc/self/exe", buf, sizeof(buf)) < 0) {
         LOGE("stat:%s", strerror(errno));
         exit(-1);
     }
-    if (getuid() == 0){
-        int fd = open("/proc/self/attr/current", O_WRONLY);
-        if (fd >= 0){
-            if (write(fd, "u:r:zygote:s0", sizeof("u:r:zygote:s0")) > 0)
-                LOGD("switch to zygote context");
-            close(fd);
-        }
+    for (int i=0;i<argc;i++) {
+    	if (argv[i] == "--zygote"sv) {
+    		zygote = (parse_ppid() == 1);
+    		break;
+    	}
     }
     char *_realpath = realpath(buf, nullptr);
     if (_realpath == nullptr) _realpath = strdup(buf);
     string bak = string(_realpath) + ".orig";
     delete _realpath;
-    if (getuid() == 0 && !mount(bak.data(), buf, nullptr, MS_BIND, nullptr)) {
+    if (zygote && getuid() == 0 && !mount(bak.data(), buf, nullptr, MS_BIND, nullptr)) {
         if (fork() == 0){
             // handle zygote restart
+            LOGD("Fork handle");
             signal(SIGUSR1, unmount_orig);
             signal(SIGTERM, SIG_IGN);
             prctl(PR_SET_PDEATHSIG, SIGUSR1);
             while (true) pause();
             _exit(1);
+        }
+        int fd = open("/proc/self/attr/current", O_WRONLY);
+        if (fd >= 0){
+            if (write(fd, "u:r:zygote:s0", sizeof("u:r:zygote:s0")) > 0)
+                LOGD("switch to zygote context");
+            close(fd);
         }
         LOGD("exec:%s", buf);
         if (execve(buf, argv, environ)) {
